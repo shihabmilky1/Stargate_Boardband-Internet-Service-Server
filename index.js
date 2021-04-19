3e6bf1d09c92bfd41754bba39aea7ae134217a6e
Shihab Milky <shihabmilky1@gmail.com>
2021-04-19 06:23:37 +0600

    bug

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser')
-const port = 5000
+const port = 3001
 require('dotenv').config();
 const MongoClient = require('mongodb').MongoClient;
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.4czm1.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://shihab5:$shihab@cluster0.4czm1.mongodb.net/$stargate?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-
+app.get('/',(req, res) =>{
+    res.send('work')
+})
 client.connect(err => {
   const serviceCollection = client.db("stargate").collection("service");
   const orderService = client.db("stargate").collection("orderService");
@@ -70,11 +72,10 @@ client.connect(err => {
         .toArray((err,document) =>{
             res.send(document)
         })
+
     })
 });
-app.get('/',(req, res) =>{
-    res.send('work')
-})
+
 
 
 app.listen(process.env.PORT || port);
